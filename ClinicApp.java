7ac340fe16d9b1c56aae0898466bf7bc983fc234
Luqman Khailmi <111408610+Soulnim@users.noreply.github.com>
2023-12-27 10:44:13 +0800

    Update ClinicApp.java

--- ClinicApp.java
+++ ClinicApp.java
@@ -45,13 +45,13 @@ public class ClinicApp
             viewAppointment();
         }
         else if (option == 'B' || option == 'b') {
-            // to be determined
+            addAppointment();
         }
         else if (option == 'C' || option == 'c') {
-            // to be determined
+            editAppointment();
         }
         else if (option == 'D' || option == 'd') {
-            // to be determined
+            deleteAppointment();
         }
         else if (option == 'E' || option == 'e') {
             // to be determined
@@ -117,6 +117,243 @@ public class ClinicApp
             }
             else if (option == 'H' || option == 'h') {
                 break;
+            }
+        }
+    }
+    
+    public static void addAppointment() {
+        System.out.print("\f");
+        System.out.println("+-------------------------------------------------------+");
+        System.out.println(" Add New Appointment");
+        System.out.println("+-------------------------------------------------------+");
+        System.out.print(" Appointment ID : ");
+        String apptID = inText.nextLine();
+        while (true) {
+            System.out.print(" Date (DD/MM/YYYY) : ");
+            String date = inText.nextLine();
+            System.out.print(" Time (24-hours format) : ");
+            String time = inText.nextLine();
+            boolean isExist = false;
+            for (int i=0;i<100;i++) {
+                Appointment current = (Appointment) apptList[i];
+                if (apptList[i] != null) {
+                    if (current.getDate().equals(date) || current.getTime().equals(time)) {
+                        isExist = true;
+                    }
+                }
+                if (apptList[i] == null) {
+                    if (isExist == false) {
+                        apptList[i] = new Appointment(apptID,date,time);
+                        System.out.print("\f");
+                        System.out.println("+-------------------------------------------------------+");
+                        System.out.println(" Data has been added!");
+                        System.out.println("+-------------------------------------------------------+");
+                        System.out.print(" Press [Enter] to continue");
+                        String enter = inText.nextLine();
+                        break;
+                    }
+                    else {
+                        System.out.println("+-------------------------------------------------------+");
+                        System.out.println(" Date or Time clashed with existing data");
+                        System.out.println("+-------------------------------------------------------+");
+                        System.out.print(" Press [Enter] to continue");
+                        String enter = inText.nextLine();
+                        System.out.print("\f");
+                        System.out.println("+-------------------------------------------------------+");
+                        System.out.println(" Add New Appointment");
+                        System.out.println("+-------------------------------------------------------+");
+                        System.out.println(" Appointment ID : " + apptID);
+                        break; // testing, need to change later
+                    }
+                }
+            }
+            if (isExist == false) {
+                break;
+            }
+        }
+    }
+    
+    public static void editAppointment() {
+        int dataStart = 0;
+        int page = 1;
+        boolean isEditing = true;
+        while (true) {
+            int counter = 0;
+            System.out.print("\f");
+            System.out.println("+-------------------------------------------------------+");
+            System.out.println(" Appointment List");
+            System.out.println("+-------------------------------------------------------+");
+            for (int i=dataStart; i<100; i++) {
+                if (counter != 10) {
+                    if (apptList[i] == null) {
+                        break;
+                    }
+                    System.out.println(" "+(i+1)+" ] "+apptList[i].toString());
+                    System.out.println("+-------------------------------------------------------+");
+                    counter++;
+                }
+                else {
+                    break;
+                }
+            }
+            System.out.println(" [ Page : "+page+" ]");
+            String option = "H";
+            if (page == 1) {
+                System.out.print(" Insert [V] Next Page, [H] Home : ");
+                option = inText.nextLine();
+            }
+            else if (page > 1) {
+                System.out.print(" Insert [C] Previous Page, [V] Next Page, [H] Home : ");
+                option = inText.nextLine();
+            }
+            if (option.equalsIgnoreCase("C")) {
+                dataStart -= 10;
+                page -= 1;
+            }
+            else if (option.equalsIgnoreCase("V")) {
+                dataStart += 10;
+                page += 1;
+            }
+            else if (option.equalsIgnoreCase("H")) {
+                isEditing = false;
+                break;
+            }
+            else {
+                try {
+                    int choice = Integer.parseInt(option);
+                    System.out.print("\f");
+                    System.out.println("+-------------------------------------------------------+");
+                    System.out.println(" Edit Appointment");
+                    System.out.println("+-------------------------------------------------------+");
+                    System.out.println(" "+(choice)+" ] "+apptList[choice-1].toString());
+                    System.out.println("+-------------------------------------------------------+");
+                    System.out.println(" 1] Edit Date");
+                    System.out.println(" 2] Edit Time");
+                    System.out.println(" 3] Back");
+                    System.out.println("+-------------------------------------------------------+");
+                    System.out.print(" Option : ");
+                    int editOpt = inNum.nextInt();
+                    boolean isEdited = false;
+                    if (editOpt == 1) {
+                        System.out.println(" Old date : "+apptList[choice-1].getDate());
+                        System.out.print(" Enter new date (DD/MM/YYYY) : ");
+                        String newDate = inText.nextLine();
+                        apptList[choice-1].setDate(newDate);
+                        isEdited = true;
+                    }
+                    else if (editOpt == 2) {
+                        System.out.println(" Old time : "+apptList[choice-1].getDate());
+                        System.out.print(" Enter new time (24-hours format) : ");
+                        String newTime = inText.nextLine();
+                        apptList[choice-1].setTime(newTime);
+                        isEdited = true;
+                    }
+                    else if (editOpt == 3) {
+                        // do nothing
+                    }
+                    if (isEdited) {
+                        System.out.print("\f");
+                        System.out.println("+-------------------------------------------------------+");
+                        System.out.println(" Data has been edited!");
+                        System.out.println("+-------------------------------------------------------+");
+                        System.out.print(" Press [Enter] to continue");
+                        String enter = inText.nextLine();
+                    }
+                }
+                catch (NumberFormatException e) {
+                    System.out.print("\f");
+                    System.out.println("+-------------------------------------------------------+");
+                    System.out.println(" Invalid Key!");
+                    System.out.println("+-------------------------------------------------------+");
+                }
+            }
+        }
+    }
+    
+    public static void deleteAppointment() {
+        int dataStart = 0;
+        int page = 1;
+        boolean isDeleting = true;
+        while (true) {
+            int counter = 0;
+            System.out.print("\f");
+            System.out.println("+-------------------------------------------------------+");
+            System.out.println(" Appointment List");
+            System.out.println("+-------------------------------------------------------+");
+            for (int i=dataStart; i<100; i++) {
+                if (counter != 10) {
+                    if (apptList[i] == null) {
+                        break;
+                    }
+                    System.out.println(" "+(i+1)+" ] "+apptList[i].toString());
+                    System.out.println("+-------------------------------------------------------+");
+                    counter++;
+                }
+                else {
+                    break;
+                }
+            }
+            String option = "H";
+            if (page == 1) {
+                System.out.println(" [ Page : "+page+" ] [V] Next Page, [H] Home");
+                System.out.print(" Choose data to be deleted (eg. 1) : ");
+                option = inText.nextLine();
+            }
+            else if (page > 1) {
+                System.out.println(" [ Page : "+page+" ] [C] Previous Page, [V] Next Page, [H] Home");
+                System.out.print(" Choose data to be deleted (eg. 1) : ");
+                option = inText.nextLine();
+            }
+            if (option.equalsIgnoreCase("C")) {
+                dataStart -= 10;
+                page -= 1;
+            }
+            else if (option.equalsIgnoreCase("V")) {
+                dataStart += 10;
+                page += 1;
+            }
+            else if (option.equalsIgnoreCase("H")) {
+                isDeleting = false;
+                break;
+            }
+            else {
+                try {
+                    int choice = Integer.parseInt(option);
+                    System.out.print("\f");
+                    System.out.println("+-------------------------------------------------------+");
+                    System.out.println(" Edit Appointment");
+                    System.out.println("+-------------------------------------------------------+");
+                    System.out.println(" "+(choice)+" ] "+apptList[choice-1].toString());
+                    System.out.println("+-------------------------------------------------------+");
+                    System.out.print(" Are you sure to delete this? (Y/N) : ");
+                    char deleteOpt = inChar.next().charAt(0);
+                    boolean isDeleted = false;
+                    if (deleteOpt == 'Y' || deleteOpt == 'y') {
+                        int count = choice-1;
+                        while (apptList[count] != null) {
+                            apptList[count] = apptList[count+1];
+                            if(apptList[count+1] == null) {
+                                apptList[count] = null;
+                            }
+                            count++;
+                        }
+                        System.out.print("\f");
+                        System.out.println("+-------------------------------------------------------+");
+                        System.out.println(" Data has been removed!");
+                        System.out.println("+-------------------------------------------------------+");
+                        System.out.print(" Press [Enter] to continue");
+                        String enter = inText.nextLine();
+                    }
+                    else {
+                        // do nothing
+                    }
+                }
+                catch (NumberFormatException e) {
+                    System.out.print("\f");
+                    System.out.println("+-------------------------------------------------------+");
+                    System.out.println(" Invalid Key!");
+                    System.out.println("+-------------------------------------------------------+");
+                }
             }
         }
     }
